4fb92a0e1a22dc0f0ce301b5283da5cfa535c898
Shafiqul Islam <shafiqruet@gmail.com>
2022-02-06 01:08:58 +0600

    update file

--- helper.php
+++ helper.php
@@ -1,5 +1,5 @@
 <?php
 
 function formatAndCleanInput($db_connection, $input) {
-			return mysqli_real_escape_string($db_connection, $input);
+	return mysqli_real_escape_string($db_connection, $input);
 }
--- job_apply.php
+++ job_apply.php
@@ -18,25 +18,24 @@ if ($_REQUEST['action'] == 'job_apply') {
     if ($numrows == 0) {
         $sql = "INSERT INTO jobs_applied (uid, job_id, created)
         VALUES ('" . $uid . "','" . $job_id . "' , NOW())";
-         $res = mysqli_query($connection, $sql);
-
-         $return["errorCode"] = 0;
-         $return["message"] = "Job Applied process is completed.";
-         $return["response_data"] = [];
-     } else {
+            $res = mysqli_query($connection, $sql);
+            $return["errorCode"] = 0;
+            $return["message"] = "Your job apply process is completed.";
+            $return["response_data"] = [];
+        } else {
+            $return["errorCode"] = - 1;
+            $return["message"] = 'You already applied this job post';
+            $return["response_data"] = [];
+        }
+    } else {
         $return["errorCode"] = - 1;
-        $return["message"] = 'Already Applied ';
+        $return["message"] = 'Send all parameters.';
         $return["response_data"] = [];
     }
-} else {
-    $return["errorCode"] = - 1;
-    $return["message"] = 'Send all parameters.';
-    $return["response_data"] = [];
-}
 
-mysqli_close($connection);
+    mysqli_close($connection);
 
-header('Content-Type: application/json');
+    header('Content-Type: application/json');
 // tell browser that its a json data
-echo json_encode($return);
+    echo json_encode($return);
 //converting array to JSON string
